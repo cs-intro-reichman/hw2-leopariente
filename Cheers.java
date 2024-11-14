34b47962acae2a59e82a3bd6e607021f7be3834c
Leo Pariente <leopariente@gmail.com>
2024-11-14 09:59:44 +0200

    Fix

--- Cheers.java
+++ Cheers.java
@@ -1,15 +1,12 @@
 // Prints a crowd cheering output.
-
-import java.util.Arrays;
-
 public class Cheers {
         public static void main(String[] args) {
             String specialLetters = "AEFHILMNORSX";
 	    String cheer = args[0];
             int totalCheers = Integer.parseInt(args[1]);
             for (int i = 0; i < cheer.length(); i++) {
-                char currentChar = cheer.charAt(i);
-                String additionWord = specialLetters.indexOf(currentChar) == -1 ? "a " : "an ";
+                char currentChar = Character.toUpperCase(cheer.charAt(i));
+                String additionWord = specialLetters.indexOf(currentChar) == -1 ? "a  " : "an  ";
                 System.out.println("Give me " + additionWord + currentChar);
             }
             System.out.println("What does that spell?");
