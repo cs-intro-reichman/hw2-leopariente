7a3caac91a15b5706cae10ccee03bc1c94de4c2d
Leo Pariente <leopariente@gmail.com>
2024-11-14 09:52:47 +0200

    Fix

--- Cheers.java
+++ Cheers.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class Cheers {
         public static void main(String[] args) {
-            char[] specialLetters = {'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'};
+            String specialLetters = "AEFHILMNORSX";
 	    String cheer = args[0];
             int totalCheers = Integer.parseInt(args[1]);
             for (int i = 0; i < cheer.length(); i++) {
                 char currentChar = cheer.charAt(i);
-                String additionWord = Arrays.asList(specialLetters).contains(currentChar) ? "an " : "a ";
+                String additionWord = specialLetters.indexOf(currentChar) == -1 ? "a " : "an ";
                 System.out.println("Give me " + additionWord + currentChar);
             }
             System.out.println("What does that spell?");
