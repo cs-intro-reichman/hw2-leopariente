16b695dea38ebf7ed0c4aa37d58af5a20d33fb59
Leo Pariente <leopariente@gmail.com>
2024-11-14 10:10:39 +0200

    Fix

--- Cheers.java
+++ Cheers.java
@@ -7,7 +7,7 @@ public class Cheers {
             for (int i = 0; i < cheer.length(); i++) {
                 char currentChar = cheer.charAt(i);
                 String additionWord = specialLetters.indexOf(currentChar) == -1 ? "a  " : "an  ";
-                System.out.println("Give me " + additionWord + currentChar);
+                System.out.println("Give me " + additionWord + currentChar + ": " + currentChar + "!");
             }
             System.out.println("What does that spell?");
             for (int i = 0; i < totalCheers; i++) {
